fe0e34e01d402b01567ad802740e2ff9d5e33d71
Zack Michener <zack@zackmichener.net>
2016-10-30 13:33:09 -0700

    create prod JS bundle

---
+++ webpack.config.prod.js
@@ -0,0 +1,35 @@
+let path = require('path');
+let webpack = require('webpack');
+
+module.exports = {
+  entry: ['./client/entry.js'],
+  output: {
+    path: path.resolve(__dirname, 'public/'),
+    publicPath: '/',
+    filename: 'app.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react', 'stage-2'],
+          cacheDirectory: false
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js']
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  ]
+};
